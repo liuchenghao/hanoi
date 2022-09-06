537da70423ab3aeb4897f5c2b0c2a620e5e022d7
liuchenghao <liuchenghao@xinhua.org>
2022-09-06 17:41:19 +0800

    clear

--- index-loop-link.js
+++ index-loop-link.js
@@ -7,10 +7,9 @@
 这一步没有明确规定移动哪个圆盘，你可能以为会有多种可能性，其实不然，可实施的行动是唯一的。
 （3）反复进行（1）（2）操作，最后就能按规定完成汉诺塔的移动。 */
 class Disc {
-  constructor(level, name, piller) {
+  constructor(level, name) {
     this.level = level || 0;
     this.name = name || this.level;
-    // this.currentPiller = this.fromPiller = piller;
   }
   getName() {
     return this.name;
@@ -18,87 +17,51 @@ class Disc {
   getLevel() {
     return this.level;
   }
-  setCurrentPiller(piller) {
-    this.currentPiller = piller;
-  }
-  setFromPiller(piller) {
-    this.fromPiller = piller;
-  }
-  getFromPiller() {
-    return this.fromPiller;
-  }
-  getCurrentPiller() {
-    return this.currentPiller;
-  }
-  move(from, to) {
-    let isAdd = to.add(this);
-    isAdd && from.remove(this);
-  }
 }
 class Piller {
   constructor(index, discs) {
     this.index = index || 0;
     this.discs = discs || [];
   }
+  // 转化piller的name
   getName() {
     let index = this.index;
     let code = 65 + index;
     return String.fromCharCode(code);
   }
   add(disc) {
-    let lastDisc = this.getLastDisc();
-    if (lastDisc) {
-      if (disc.level < lastDisc.level) {
-        this.discs.push(disc);
-        return true;
-      }
-    } else {
-      this.discs.push(disc);
-      return true;
-    }
-    return false;
+    this.discs.push(disc);
   }
   getLastDisc() {
     let lastIndex = this.discs.length - 1;
     return this.discs[lastIndex];
   }
-  remove(disc) {
-    let index = this.discs.indexOf(disc);
-    ~index && this.discs.splice(index, 1);
-  }
-  move() {
-
+  remove() {
+    return this.discs.pop();
   }
   moveTo(to) {
+    let countF = this.getDiscCount();
+    let countT = to.getDiscCount();
+    if (countF) {
+      if (countT) {
+        let lastF = this.getLastDisc();
+        let lastT = to.getLastDisc();
+        if (lastT.getLevel() < lastF.getLevel()) {
+          return false;
+        }
+      }
+      let last = this.remove();
+      to.add(last);
+      console.info(this.getName(), " -> ", to.getName())
+      // console.info(this.getName(), " -> ", to.getName(),this,  to)
+      return true;
+    } else {
+      return false;
+    }
   }
   getDiscCount() {
     return this.discs.length;
   }
-}
-let piller = ['a', 'c', 'b']
-let disc = [[3, 2, 1], [], []];
-function move(from, to) {
-  let discF = disc[from];
-  let discT = disc[to];
-  if (discF.length) {
-    // console.info(lastF,from,"======", to, discF)
-    if (discT.length) {
-      let lastF = discF[discF.length - 1];
-      let lastT = discT[discT.length - 1];
-      if (lastT < lastF) {
-        return false;
-      }
-    }
-    let last = discF.pop();
-    discT.push(last);
-    console.info(piller[from], " -> ", piller[to], disc, last)
-    return true;
-  } else {
-    return false;
-  }
-  // if(disc[to]){
-
-  // }
 }
 class Node {
   constructor(data, next, prev) {
@@ -124,11 +87,7 @@ class Node {
 }
 class LinkedList {
   constructor(num, isCreate) {
-    // this.next = next;
-    // this.prev = prev;
-    // this.data = data;
     this.num = num;
-    // this.root = this.init(num);
     isCreate && (this.root = this.create());
   }
   create() {
@@ -139,26 +98,13 @@ class LinkedList {
     for (let index = 0; index < num; index++) {
       let node = this.getNode(index);
       if (index) {
-        /* next.setNext(node);
-        // next.setPrev(prev);
-        node.setNext(root);
-        // node.setPrev()
-        next = node;
-        prev = node; */
         node.setPrev(next);
         next.setNext(node);
         next = node;
       } else {
-        /* root = node;
-        root.setNext(root);
-        root.setPrev(root);
-        next = root;
-        prev = root; */
         root = node;
         next = node;
       }
-      // let node = this.getNode(index);
-      // let next = this.getNode(index + 1)
     }
     // 首尾相接构造环状链表
     next.setNext(root);
@@ -190,95 +136,26 @@ class LinkedPiller extends LinkedList {
       }
       discs.reverse();
     }
-    // console.info(this, discs, this.level)
     return new Piller(index, discs);
   }
   move(from, to) {
-    let discF = from.data.discs;
-    let discT = to.data.discs;
-    // console.info(from,"======", to, discF)
-
-    if (discF.length) {
-      if (discT.length) {
-        let lastF = discF[discF.length - 1].level;
-        let lastT = discT[discT.length - 1].level;
-        if (lastT < lastF) {
-          return false;
-        }
-      }
-      let last = discF.pop();
-      discT.push(last);
-      console.info(from.data.getName(), " -> ", to.data.getName())
-      // console.info(from.data.getName(), " -> ", to.data.getName(), from.data, to.data)
-      return true;
-    } else {
-      return false;
-    }
-    // if(disc[to]){
-  
-    // }
+    let pillerF = from.data;
+    let pillerT = to.data;
+    return pillerF.moveTo(pillerT);
   }
 }
 function init() {
-  let count = 0;
-  // std::cin >> N;
-  // for (int i = 0; i < N; i++)
-  // 	a[1].push(N - i);
-  // if (N % 2 == 1) {
-  // 	s[2] = 'c'; s[3] = 'b';
-  // }
-  while (++count) {
-    let index = 0;
-    let from = (count - 1) % 3 + index;
-    let to = (count) % 3 + index;
-    let buffer = (count + 1) % 3 + index;
-    console.info(count)
-    move(from, to);
-    if (!move(from, buffer) && !move(buffer, from)) break;
-  }
-}
-function init1() {
-  let link = new LinkedPiller(3, 6);
+  let rank = 3;
+  let level = 3;
+  let link = new LinkedPiller(rank, level);
   let root = link.root;
   let move = link.move;
-  let count = 0;
-  while ( root = root.getPrev()) {
-    // let index = 0;
+  while (root = root.getPrev()) {
     let from = root.getNext();
     let to = root;
     let buffer = root.getPrev();
-    // console.info(count, from, to, buffer)
     move(from, to);
     if (!move(from, buffer) && !move(buffer, from)) break;
   }
-  // console.info(link.root)
-  // console.info(link.root.next.data.getName())
 }
-init1();
-/* char s[4] = { 'q', 'a', 'b', 'c' };
-std:: stack < int > a[4];
-bool move(int before, int after) {
-  if (a[before].empty())
-    return false;
-  if (!a[after].empty())
-    if ((a[after].top() - a[before].top()) < 0)
-      return false;
-  a[after].push(a[before].top());
-  a[before].pop();
-  printf("%c -> %c\n", s[before], s[after]);//faster than cout
-  return true;
-}
-int main() {
-  int  N, count = 0;
-  std:: cin >> N;
-  for (int i = 0; i < N; i++)
-  a[1].push(N - i);
-  if (N % 2 == 1) {
-    s[2] = 'c'; s[3] = 'b';
-  }
-  while (++count) {
-    move((count - 1) % 3 + 1, (count) % 3 + 1);
-    if (!move((count - 1) % 3 + 1, (count + 1) % 3 + 1) && !move((count + 1) % 3 + 1, (count - 1) % 3 + 1))
-      break;
-  }
-} */
+init();
